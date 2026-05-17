126b65e398937be168d5e94d20854581fe0c5cb9
Илья Шестак <ilia1259697@gmail.com>
2026-05-17 17:36:23 +0300

    creating decks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const DeckForm = ({ decks, activeDeckNum, onChangeDeck }) => {
+  return (
+    <>
+      {decks.map((deck, index) => {
+        if (index === activeDeckNum)
+          return <input key={deck.id} defaultValue={deck.name}></input>;
+        else
+          return (
+            <button
+              key={deck.id}
+              onClick={() => {
+                onChangeDeck(index);
+              }}
+            >
+              {deck.name}
+            </button>
+          );
+      })}
+    </>
+  );
+};
+
 const createCard = (front, back) => ({
   id: crypto.randomUUID(),
   frontSide: front,
   backSide: back,
 });
-const createDeck = (name, cards) => ({ id: crypto.randomUUID(), cards: cards });
 
-function CardForm({ onAddCard }) {
+const createDeck = (name, cards) => ({
+  id: crypto.randomUUID(),
+  name: name,
+  cards: cards,
+});
+
+const ErrorMessage = ({ text }) => {
+  if (!text) return null;
+  return <p style={{ color: "red", fontSize: "12px" }}>{text}</p>;
+};
+
+function CardForm({ onAddSingleCard, onAddManyCards }) {
   const [frontSideText, setFrontSideText] = useState("");
   const [backSideText, setBackSideText] = useState("");
   const [errMessage, setErrMessage] = useState("");
@@ -16,36 +48,35 @@ function CardForm({ onAddCard }) {
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=50")
       .then((response) => response.json())
-      .then((data) => data.results)
-      .then((results) =>
-        results.map((element) => {
-          onAddCard(element.question, element.correct_answer);
-        }),
-      );
+      .then((data) => {
+        const newCards = data.results.map((element) =>
+          createCard(element.question, element.correct_answer),
+        );
+        onAddManyCards(newCards);
+      });
   }, []);
 
   return (
     <>
       <div>
         <input
-          onChange={(e) => {
-            setFrontSideText(e.target.value);
-          }}
+          onChange={(e) => setFrontSideText(e.target.value)}
           value={frontSideText}
+          placeholder="Front Side"
         ></input>
         <input
-          onChange={(e) => {
-            setBackSideText(e.target.value);
-          }}
+          onChange={(e) => setBackSideText(e.target.value)}
           value={backSideText}
+          placeholder="Back Side"
         ></input>
       </div>
-      <p style={{ color: "red", fontSize: "12px" }}>{errMessage}</p>
-      {/*вынести err как отдельный компонент*/}
+
+      <ErrorMessage text={errMessage} />
+
       <button
         onClick={() => {
           if (frontSideText.trim() && backSideText.trim()) {
-            onAddCard(frontSideText, backSideText);
+            onAddSingleCard(frontSideText, backSideText);
             setFrontSideText("");
             setBackSideText("");
           } else {
@@ -65,30 +96,57 @@ function CardForm({ onAddCard }) {
 }
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [decks, setDecks] = useState([]);
-  const [deckNum, setDeckNum] = useState(0);
-  const handleAddCard_ = (deckNum, front, back) => {
+  const [decks, setDecks] = useState(() => [
+    createDeck("asd", [createCard("asd", "qwe")]),
+  ]);
+  const [activeDeckNum, setDeckNum] = useState(0);
+
+  const handleAddCard = (front, back, deckNum = activeDeckNum) => {
     setDecks((prevDecks) =>
       prevDecks.map((deck, index) => {
         if (index !== deckNum) return deck;
-        else
-          return {
-            ...deck,
-            cards: [...deck.cards, createCard(front, back)],
-          };
+        return {
+          ...deck,
+          cards: [...deck.cards, createCard(front, back)],
+        };
       }),
     );
   };
 
-  const handleAddCard = (front, back) => {
-    setCards((prevCards) => [...prevCards, createCard(front, back)]);
+  const handleAddCards = (newCards, deckNum = activeDeckNum) => {
+    setDecks((prevDecks) =>
+      prevDecks.map((deck, index) => {
+        if (index !== deckNum) return deck;
+        return {
+          ...deck,
+          cards: [...deck.cards, ...newCards],
+        };
+      }),
+    );
   };
 
   return (
     <>
-      <CardForm onAddCard={handleAddCard} />
-      {cards.map((card) => (
+      <button
+        onClick={() => {
+          setDecks([...decks, createDeck("new deck", [])]);
+        }}
+      >
+        create deck
+      </button>
+
+      <DeckForm
+        decks={decks}
+        activeDeckNum={activeDeckNum}
+        onChangeDeck={setDeckNum}
+      />
+
+      <CardForm
+        onAddSingleCard={handleAddCard}
+        onAddManyCards={handleAddCards}
+      />
+
+      {decks[activeDeckNum]?.cards.map((card) => (
         <div className="card" key={card.id}>
           <div
             style={{ width: "50%" }}
